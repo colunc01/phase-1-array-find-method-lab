d720db902176c323a8b2fe4446975572874451cf
Cesar Colunga <cesara.colunga@gmail.com>
2023-02-07 13:19:50 -0600

    solved the problem

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// code your solution here
+// code your solution here [year, result]
+
+function isWin(record){
+    return record.result === 'W';
+  
+}
+
+// Function superBowlWin() receives 1 argument; an Array of JavaScript Object
+function superbowlWin(record){
+    let winningYear = 0;
+    winningYear = record.find(isWin);
+
+    if (winningYear === undefined)
+    {
+        return winningYear;
+    }
+    else
+    {
+        return winningYear.year;
+    }
+   
+}
